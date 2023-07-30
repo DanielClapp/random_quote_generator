7ab4bb588401c303953c1555531a89963a01d7ff
Daniel Clapp <danielclapp94@gmail.com>
2023-07-30 13:59:51 -0700

    fixed a typo, no major changes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const quoteContainer = document.getElementById('quote-container');
 const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
-const TwitterBtn = document.getElementById('twitter');
+const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
 const loader = document.getElementById('loader');
 
@@ -77,7 +77,7 @@ newQuoteBtn.addEventListener('click', function (e) {
   document.body.style.backgroundColor = newColor;
 });
 
-TwitterBtn.addEventListener('click', tweetQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
 getQuotes();
